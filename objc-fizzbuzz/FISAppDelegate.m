59338931a8482230528fd77b61ea3d67da316be1
molgin <mollygingras@gmail.com>
2016-01-03 17:08:07 -0800

    fizz the buzz

--- objc-fizzbuzz/FISAppDelegate.m
+++ objc-fizzbuzz/FISAppDelegate.m
@@ -7,11 +7,20 @@
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
     
-    /*
-     
-     * Write your code here!
-     
-     */
+    for (NSUInteger i = 1; i <= 100; i++) {
+        BOOL divisibleBy3 = i % 3 == 0;
+        BOOL divisibleBy5 = i % 5 == 0;
+        if (divisibleBy3 && divisibleBy5) {
+            NSLog(@"FizzBuzz");
+        } else if (divisibleBy3) {
+            NSLog(@"Fizz");
+        } else if (divisibleBy5) {
+            NSLog(@"Buzz");
+        }
+        else {
+            NSLog(@"%li", i);
+        }
+    }
     
     // do not alter
     return YES;  //
